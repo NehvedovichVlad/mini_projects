0284bf731fb02743d2709c3a5e97443e8f51bdde
Uladzislau Nekhviadovich <vladislavnehvedovich@gmail.com>
2021-01-04 19:31:23 +0300

    moves rook, pawn and knight

--- Chess/Chess/ChessEngine.py
+++ Chess/Chess/ChessEngine.py
@@ -19,9 +19,11 @@ class GameState():
             ["--", "--", "--", "--", "--", "--", "--", "--"],
             ["--", "--", "--", "--", "--", "--", "--", "--"],
             ["--", "--", "--", "--", "--", "--", "--", "--"],
-            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "bp", "--", "--", "--"],
             ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
             ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"], ]
+        self.moveFunctions = {'p': self.getPawnMoves, 'R': self.getRookMoves, 'N': self.getKnightMoves,
+                              'B': self.getBishopMoves, 'Q': self.getQueenMoves, 'K': self.getKingMoves}
         self.whiteToMove = True
         self.moveLog = []
 
@@ -47,24 +49,86 @@ class GameState():
     ''' Все возможные ходы без учета проверок'''
 
     def getAllPossibleMoves(self):
-        moves = [Move((6, 4), (4, 4), self.board)]
+        moves = []
         for r in range(len(self.board)):
             for c in range(len(self.board[r])):  # Номер колонки в полученной строке
                 turn = self.board[r][c][0]
-                if (turn == 'w' and self.whiteToMove) and (turn == 'b' and not self.whiteToMove):
+                if (turn == 'w' and self.whiteToMove) or (turn == 'b' and not self.whiteToMove):
                     piece = self.board[r][c][1]
-                    if piece == 'p':
-                        self.getPawnMoves(r, c, moves)
-                    elif piece == 'R':
-                        self.getRookMoves(r, c, moves)
+                    # вызывает соответсвующие функции перемещения в зависимоти от типа фигуры
+                    self.moveFunctions[piece](r, c, moves)
         return moves
 
     # получить все возможные ходы для пешки и добавить эти действие в список
     def getPawnMoves(self, r, c, moves):
-        pass
+        if self.whiteToMove: # если белые ходят
+            if self.board[r-1][c] == "--":
+                moves.append(Move((r, c), (r-1, c), self.board))
+                if r == 6 and self.board[r-2][c] == "--":
+                    moves.append(Move((r, c), (r-2, c), self.board))
+            if c-1 >= 0: # захват слева
+                if self.board[r-1][c-1][0] == 'b': # враг захвачен
+                    moves.append(Move((r, c), (r-1, c-1), self.board))
+            if c+1 <= 7: # захват справа
+                if self.board[r-1][c+1][0] == 'b': # враг захвачен
+                    moves.append(Move((r, c), (r-1, c+1), self.board))
+
+        else: # если черные ходят
+            if self.board[r+1][c] == "--":
+                moves.append(Move((r, c), (r+1, c), self.board))
+                if r == 1 and self.board[r+2][c] == "--":
+                    moves.append(Move((r, c), (r+2, c), self.board))
+
+            if c-1 >= 0: # захват слева
+                if self.board[r+1][c-1][0] == 'w': # враг захвачен
+                    moves.append(Move((r, c), (r+1, c-1), self.board))
+            if c+1 <= 7: # захват справа
+                if self.board[r+1][c+1][0] == 'w': # враг захвачен
+                    moves.append(Move((r, c), (r+1, c+1), self.board))
 
     # получить все возможные ходы для ладьи и добавить эти действие в список
     def getRookMoves(self, r, c, moves):
+        directions = ((-1, 0), (0, -1), (1, 0), (0, 1))
+        enemyColor = "b" if self.whiteToMove else "w"
+        for d in directions:
+            for i in range(1, 8):
+                endRow = r + d[0] * i
+                endCol = c + d[1] * i
+                if 0 <= endRow < 8 and 0 <= endCol < 8: # на доске
+                    endPiece = self.board[endRow][endCol]
+                    if endPiece == "--":
+                        moves.append(Move((r, c), (endRow, endCol), self.board))
+                    elif endPiece[0] == enemyColor:
+                        moves.append(Move((r, c), (endRow, endCol), self.board))
+                        break
+                    else:
+                        break
+                else: # off board
+                    break
+
+
+    # получить все возможные ходы для коня и добавить эти действие в список
+    def getKnightMoves(self, r, c, moves):
+        knightMoves = ((-2, -1), (-2, 1), (-1, -2), (-1, 2), (1, -2), (1, 2), (2, -1), (2, 1))
+        allyColor = "w" if self.whiteToMove else "b"
+        for m in knightMoves:
+            endRow = r + m[0]
+            endCol = c + m[1]
+            if 0 <= endRow < 8 and 0 <= endCol < 8:
+                endPiece = self.board[endRow][endCol]
+                if endPiece[0] != allyColor: # не союзник ( пустой кусок )
+                    moves.append(Move((r, c), (endRow, endCol), self.board ))
+
+    # получить все возможные ходы для слона и добавить эти действие в список
+    def getBishopMoves(self, r, c, moves):
+        pass
+        # получить все возможные ходы для королевы и добавить эти действие в список
+
+    def getQueenMoves(self, r, c, moves):
+        pass
+        # получить все возможные ходы для короля и добавить эти действие в список
+
+    def getKingMoves(self, r, c, moves):
         pass
 
 
@@ -92,7 +156,7 @@ class Move():
     def __eq__(self, other):
         if isinstance(other, Move):
             return self.moveID == other.moveID
-        return  False
+        return False
 
     def getChessNotation(self):
         return self.getRankFile(self.startRow, self.startCol)
